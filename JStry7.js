63166be498f3ee183c774f80b21ea4c1bcf8a39a
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-09-04 18:33:43 +0800

    faluse

--- JStry7.js
+++ JStry7.js
@@ -1,31 +1,59 @@
-var $text = $(".text").val();//計算機的數字
-console.log($text);
+var operand = [];//[123+456]
+var operator = '';//+.-.*./.=
+var display = 0;//機算機顯示的數字
+var isDec = false;
+var squ = 0;
+var userClick = false;
+
+
+var $num = $(".answer>input").val();//計算機的數字
 var $add;
 var $minus;
 var $multiply;
 var $divided;
-var total;
+var total = 0;
+var userClick = false;
 $(function () {
-    $(".btn").click(function () {
+    //先放0進去
+    $(".answer>input").val(display);
+    //按下but先執行是數字還是運算
+    $("button").click(function () {
         var num = $(this).val();
-        if (num == "." && $text < 1) { $text += num; $(".text").val($text); console.log("A"); } else {
-            switch (num) {
-                case "clean": $text = 0; num = $text; $(".text").val($text); break;
-                case "del": $text = $text.substring(0, $text.length - 1); $(".text").val($text.substr(1, 20)); break;
-                case "+": add(); answer(); break;
-                case "answer":
-                default:
-                    $text += num;
-                    $(".text").val($text.substr(1, 20)); console.log($text.substr(1, 20));
-            }
+        switch (num) {
+            case "clean":
+                display = 0;
+                squ = 0;
+                isDec = false;
+                userClick = false;
+                $(".answer>input").val(display);
+                break;
+
+            case "del": var $num = (".answer>input").val(); $num = $num.substring(0, $num.length - 1); $(".answer>input").val($num.substr(1, 20)); break;
+
+            //處理中
+            case "+": case "-": case "*": case "/": calculator(num); break;
+            case ".": if (!userClick) { userClick = true; $(".answer>input").val(display + '.'); } break;
+            case "answer": answer(); break;
+            default:
+               
+                $num += num;
+                $(".answer>input").val($num.substr(1, 20));
+
+
         }
-    });
-});
+        console.log($num);
+        console.log(num);
 
+    })
 
-//hover
-var $button = $(".btn");
-$button.hover(function () {
-    $(this).css({ "opacity": "0.5" });
-}, function () { $(this).css({ "opacity": "1" }); });
+    function calculator() {
 
+    }
+
+    //hover
+    var $button = $(".btn");
+    $button.hover(function () {
+        $(this).css({ "opacity": "0.5" });
+    }, function () { $(this).css({ "opacity": "1" }); });
+
+})
