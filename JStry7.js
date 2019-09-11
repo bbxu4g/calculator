8eedd4634c6f90c39a7ae3f034cb44c035d79ce0
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-09-11 17:49:47 +0800

    V1

--- JStry7.js
+++ JStry7.js
@@ -4,10 +4,8 @@ var display = 0;//機算機顯示的數字
 var isDec = false;
 var squ = 0;
 var userClick = false;
-
-
 var $num = $(".answer>input").val();//計算機的數字
-var total = 0;
+
 $(function () {
     //先放0進去
     $(".answer>input").val(display);
@@ -16,73 +14,98 @@ $(function () {
     $("button").click(function () {
 
         var num = $(this).val();//計算機的數字
-        switch (num) {
-            case "clean":
-                display = 0;
-                squ = 0;
-                isDec = false;
-                userClick = false;
-                $num = 0;
-                operand=[];
-                $(".answer>input").val(display);
-                break;
-            case "del":
+        if ($(".answer>input").val().length > 14) {alert("超過15個字了");$(".answer>input").val($num.substring(1,15));}
+        else {
+            switch (num) {
+                case "clean":
+                    display = 0;
+                    squ = 0;
+                    isDec = false;
+                    userClick = false;
+                    $num = 0;
+                    operand = [];
+                    $(".answer>input").val(display);
+                    break;
+                case "del":
+                    $num = $num.substring(0, $num.length - 1);
+                    $(".answer>input").val($num.substring(1, $num.length));
+                    if ($num == 0) $(".answer>input").val("0");
+                    break;
+                case "+": case "-": case "*": case "/": calculator(num); break;
+                case ".": dot(); break;
+                case "answer": if ($num != "0") { operand.push($num); answer(); } break;
+                default:
+                    if ($num === '0' && num == 0) { $(".answer>input").val("0"); }
+                    else {
 
-                $num = $num.substring(0, $num.length - 1);
-                $(".answer>input").val($num.substring(1, $num.length));
-                if ($num == 0) $(".answer>input").val("0");
-                break;
+                        if (userClick == false) { displayNumber(num); console.log("A"); }
+                        else { console.log("B"); $num = $(".answer>input").val(); operand.push($num); $num = num; $(".answer>input").val(num); }
 
-            //處理中
-            case "+": case "-": case "*": case "/": calculator(num); break;
-            case ".": if (!userClick) { userClick = true; $(".answer>input").val(display + '.'); } break;
-            case "answer": answer(); break;
-            default:
-                if (userClick == false) displayNumber(num);
-                else { userClick = false; $num = 0; $num += num; $(".answer>input").val(num); }
-                break;
+                    }
+                    break;
+
+            }
 
         }
-
-
     })
+
+
+    function dot() {
+        if (!isDec) {
+            isDec = true;
+            if ($(".answer>input").val() === "0") { $num = "0."; }
+            else { $num = $num.substr(1, length) + '.'; }
+            $(".answer>input").val($num);
+        }
+    }
+    function answer() {
+        console.log(squ);
+        calc(squ);
+    }
+
     function calc(e) {
         console.log(e);//符號
-        var operand_01 = operand.pop()*1;
-        var operand_02 = operand.pop()*1;
+        var operand_01 = operand.pop() * 1;
+        var operand_02 = operand.pop() * 1;
         console.log(operand_01, operand_02);
         switch (e) {
             case "+":
-display=operand_01*1+operand_02*1;
-console.log(display);
-$(".answer>input").val(display);
-operand.push(display);
-
+                display = operand_01 + operand_02;
+                $(".answer>input").val(display);
                 break;
             case "-":
+                display = operand_02 - operand_01;
+                $(".answer>input").val(display);
                 break;
             case "*":
+                display = operand_02 * operand_01;
+                $(".answer>input").val(display);
                 break;
             case "/":
+                display = operand_02 / operand_01;
+                console.log(display);
+                $(".answer>input").val(parseInt(display) );
                 break;
         }
     }
-    //存數字
+    //按下四則運算則存數字
     function calculator(num) {
-        operand.push(display.substr(0, $num.length ));
-        console.log(operand);// [empty, "123", "456"]
-
-        // console.log(operand,operator);
-        // operand[1] = operator;// [empty, "123"] "456"
-        // console.log(operand,operator);
-       
-        userClick = true;
-        if(operand.length>1) calc(num);
-        //  console.log(operand);// [empty, "456"]
-       
+        // console.log(operand, $num, num, userClick);
+        $num = $(".answer>input").val();
+        squ = num;
+        if (userClick == false) {
+            userClick = true;
+            operand.push($num);
+            console.log("A");
+        }
+        else {
+            console.log("B");
+            if (operand.length > 1) {  operand.push($num); calc(num); console.log("C"); }
+            else { operand = []; operand.push($num); console.log("D"); }
+        }
 
     }
-
+    //單純的數字
     function displayNumber(num) {
         $num += num;
         display = $num;
