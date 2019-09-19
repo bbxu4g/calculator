2bfb1172ecd751d62d3d4585544a58e1a1e263e9
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-09-19 18:33:42 +0800

    Update JStry7.js

--- JStry7.js
+++ JStry7.js
@@ -64,20 +64,20 @@ $(function () {
     function dot() {
         $num = $(".answer>input").val();
         if ($num % 1 === 0) {
-            if (userClick != false && num3== "") { $(".answer>input").val("0.");$num = "0.";}
+            if (userClick != false && num3== ""&&$num=="") { $(".answer>input").val("0.");$num = "0.";  }
             else {
                 if (isDec == false) {
                     isDec = true;
                     if ($(".answer>input").val() === "0") { $num = "0."; console.log($num); }
                     else {
-                        $num = $num + '.';
+                        $num = $num + '.';  
                     }
-                    $(".answer>input").val($num); ;
+                    $(".answer>input").val($num);   
 
                 }
             }
         }
-        else if(userClick != false&&num3==""){$(".answer>input").val("0.");$num=$(".answer>input").val();}
+        else if(userClick != false&&num3==""){$(".answer>input").val("0.");$num=$(".answer>input").val();   console.log("73"); }
     }
 
 
