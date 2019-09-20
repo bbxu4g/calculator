d0bb995891095872269a41d2c552bca7f21de399
bbxu4g <52648679+bbxu4g@users.noreply.github.com>
2019-09-20 16:32:42 +0800

    Update JStry7.js

--- JStry7.js
+++ JStry7.js
@@ -17,7 +17,7 @@ $(function () {
     $("button").click(function () {
 
         var num = $(this).val();//計算機的數字
-        if ($(".answer>input").val().length > 14) { alert("超過15個字了"); $num = $(".answer>input").val(); $num = $num.substring(0, $num.length); $(".answer>input").val($num); }
+        if ($(".answer>input").val().length > 14) { alert("超過15個字了"); $num = $(".answer>input").val(); $num = $num.substring(0, 15); $(".answer>input").val($num); }
         switch (num) {
             case "clean":
                 display = 0;
@@ -30,23 +30,18 @@ $(function () {
                 $(".answer>input").val(display);
                 break;
             case "del": del(); break;
-            case "+": case "-": case "*": case "/": if (_exist == 0) { calculator(num); } else { _exist = 1; calculator(num); } break;
+            case "+": case "-": case "*": case "/": if ($(".answer>input").val() == "0") { $num = "-"; $(".answer>input").val($num); } if (_exist == 0) { calculator(num); } else { _exist = 1; calculator(num); } break;
             case ".": dot(); break;
             case "answer": if (_equl == 0) { $num = $(".answer>input").val(); if ($num != "0") { operand.push($num); answer(); } _equl = 1; } break;
             default:
                 _exist = 0;
                 _equl = 0;
                 if ($num === '0' && num == 0) { $(".answer>input").val("0"); }
-                else {
-                    if (userClick == false) {
-                        if ($(".answer>input").val() == "") { $(".answer>input").val("0"); } displayNumber(num);
-                    }//還沒按過符號
-                    else {
-                        $(".answer>input").val("");
-                        displayNumber(num); console.log(num);
-                    }
-                }
-                break;
+                else { if ($(".answer>input").val() == "") { $(".answer>input").val("0"); } }
+                displayNumber(num); break;
+
+
+
         }
     })
 
@@ -104,11 +99,6 @@ $(function () {
 
     }
 
-
-
-
-
-
     function answer() { //重複按等於的不執行
         if (operand.length > 1 && _exist == 0) { calc(squ); num3 = ""; } operand = []; userClick = false; console.log(operand);
     }
@@ -147,33 +137,34 @@ $(function () {
     function calculator(num) {
         $num = $(".answer>input").val();   //$num計算機上的數字
         console.log(num);                  //num是符號
-
-        if (userClick == false) { //還沒按過符號
-            // operand = [];
-            $num = $num * 1;
-            operand.push($num);//先推一個數字在推計算機上的數字[123]
-            squ = num;         //符號存起來
-            _exist = 1;
-            console.log(operand);
-            userClick = true;
-        }
-        else {
-            $num = $num * 1;
-            operand.push($num * 1);
-            console.log(operand);
-            if (operand.length > 1) {
-                if (_exist == 0) {
-                    _exist = 1;//避免重複的加減乘除
-                    calc(squ);
-                    num3 = "";
-                }
-                squ = num;//存第二個符號
+        if ($(".answer>input").val() != "-") {
+            if (userClick == false) { //還沒按過符號
+                // operand = [];
+                $num = $num * 1;
+                operand.push($num);//先推一個數字在推計算機上的數字[123]
+                squ = num;         //符號存起來
+                _exist = 1;
+                console.log(operand);
+                userClick = true;
             }
+            else {
+                $num = $num * 1;
+                operand.push($num * 1);
+                console.log(operand);
+                if (operand.length > 1) {
+                    if (_exist == 0) {
+                        _exist = 1;//避免重複的加減乘除
+                        calc(squ);
+                        num3 = "";
+                    }
+                    squ = num;//存第二個符號
+                }
+            }
+            display = 0;
+            isDec = false;
+            num3 = "";
+            $num = "";
         }
-        display = 0;
-        isDec = false;
-        num3 = "";
-        $num = "";
     }
 
     //單純的數字
@@ -190,14 +181,14 @@ $(function () {
 
         else {//按+-*/後
             if (isDec == true) {
-                if ($(".answer>input").val() === "0.") { $num = "0."; }
+                if ($(".answer>input").val() === "0.") { $num = "0."; console.log("A"); }
             }
             else {
-                if ($(".answer>input").val() === "0") { $num = ""; }
-                else if ($(".answer>input").val() == "") { $num = ""; }
+                if ($(".answer>input").val() === "0") { $num = ""; console.log("A"); }
+                else if ($(".answer>input").val() == "") { $num = ""; console.log("A"); }
             }
             $num += num;
-            $(".answer>input").val($num);
+            $(".answer>input").val($num); console.log("A");
 
         }
     }
@@ -207,5 +198,10 @@ $(function () {
     $button.hover(function () {
         $(this).css({ "opacity": "0.5" });
     }, function () { $(this).css({ "opacity": "1" }); });
+
+    $(".answer>input").keypress(function (e) {
+        if (e.keyCode == 13) { $(".btn").click(); };
+
+    });
 
 })
